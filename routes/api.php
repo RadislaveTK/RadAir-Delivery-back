e5b1c65d2408069a52ae2076236fbcddf2538d11
RadislaveTK <tkachenkoradislav@gmail.com>
2025-08-10 05:06:49 +0500

    1.5.4

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
 
 Route::get("/category/create/{name}", [CategoryController::class, 'create']);
 Route::get("/category", [CategoryController::class, 'show']);
@@ -14,12 +13,3 @@ Route::get("/category/{slug}/create_product", [ProductController::class, 'create
 Route::get("/product", [ProductController::class, 'show']);
 
 Route::get("/product/search", [ProductController::class, 'search']);
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::post('/logout',  [UserController::class, 'logout'])->middleware('auth:sanctum');
-
-
-Route::post('/register', [UserController::class, 'register'])->name('register');
-Route::post('/login', [UserController::class, 'login'])->name('login');
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,17 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
 
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
